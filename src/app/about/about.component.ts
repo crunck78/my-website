60d53ecb24fd37e57e1b3e442f27731859ff905c
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2020-10-20 20:57:10 +0200

    adding paragraphs to about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,23 +1,34 @@
 import { ElementRef } from '@angular/core';
 import { ViewChild } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { CanvasSpace, Create, Group, Line, Polygon, Pt, Rectangle } from 'pts';
+import { paragraphs } from '../paragraphs';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AboutComponent implements OnInit, AfterViewInit {
   @ViewChild("about") private parentRef: ElementRef<HTMLElement>;
   aboutElement: HTMLElement;
 
+  paragraphs = paragraphs;
+ 
   constructor() { }
 
   ngOnInit(): void {
 
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.aboutElement = this.parentRef.nativeElement;
+    let paragraphContainers = this.aboutElement.querySelectorAll('.text-mask');
+    console.log(paragraphContainers);
+    paragraphContainers.forEach( (container, index) =>{
+        container.insertAdjacentHTML( 'beforeend', this.paragraphs[index]);
+    });
   }
 }
---
+++ src/app/paragraphs.ts
@@ -0,0 +1,31 @@
+export const paragraphs = [
+    `<p>Hi, my Name is Mihai. I'm 30 years old. I leave in Germany and my passion is <b>programming Computers</b>.</p>`,
+
+    `<p>I was born in <b>Brasov, Romania</b>. I graduated in a technological high school as a <b>computing technician Operator</b>.</p>`,
+
+    `<p>Back in 2010, after school, i traveled to my mother in Germany. She lives in <b>Rosenheim, Bavaria</b>.
+        There I follow a Curse on German Language for Immigrants working beside a Restaurant as a Dishwasher.
+    </p>`,
+
+    `<p>After graduating the Language Curse with a B1 Degree in 2011, I started working at a Factory, operating <b>CNC cutting machines</b>.</p>`,
+
+    `<p>At this point I've started to self educate myself on programming computers.
+        Started by following tutorials on the internet and reading books.
+        The most used source of learning was the <b><a href="https://www.cprogramming.com/" target="_blank">cprogramming.com</a></b> website. And the book <b><i>"Jumping into C++"</i></b> by <b>Alex Allain</b>.
+    </p>`,
+
+    `<p>After finishing the book I started to learn graphics programming following the tutorials on <b><a href="https://learnopengl.com/" target="_blank">learnopengl.com website</a></b>.
+        There I come across the <b><a href="https://www.khanacademy.org/" target="_blank">Khan Academy Platform</a></b>. A free online education program. Where I start learning Mathematics, Computer Science and Computer Programming.
+    </p>`,
+
+    `<p>In March 2015 my daughter <b><i>Sofia Teodora</i></b> was born, and later that year I graduated my professional training as a <b>Machine and Production Facility Operator</b>.
+        From there on I worked as a driver on a Forklift truck until today.
+    </p>`,
+
+    `<p>At mid of this year I was recruited by the <b><a href="https://developerakademie.com/" target="_blank">Developer Akademie Team</a></b>. They trained me to become a <b>Front-end Developer</b>.
+        Here I learn to build websites through intensive coaching, online seminars, video conferences.
+        Today I learned to develop websites using the <b>Angular web platform framework</b>.
+    </p>`,
+
+    `<p>Besides programming, I am also interested in social and economic movements. In particular <b><a href="https://www.thevenusproject.com/" target="_blank">The Venus Project Organisation</a></b>.</p>`
+];
