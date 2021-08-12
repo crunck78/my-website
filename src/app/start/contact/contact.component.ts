3e1376c4a07fcd8fe7af03bfca58f4f4e9eca6bc
Crunck78 <crunck78@googlemail.com>
2021-08-12 20:39:47 +0200

    hide routes

--- src/app/start/contact/contact.component.ts
+++ src/app/start/contact/contact.component.ts
@@ -9,50 +9,52 @@ import { NavigationService } from 'src/app/services/navigation.service';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent{
 
   constructor(
     private http: HttpClient,
     private progressBar: ProgressBarService,
     public navigation: NavigationService) { }
 
-  //endpoint = "http://localhost:4200/src/app/sendEmail.php";
   endpoint = "https://mihai-andrei-neacsu.developerakademie.com/email/sendMail.php";
-
   submitReport: string;
   submitComplete = false;
   resultColor: string;
 
   onSubmit(myForm: NgForm) {
-    this.progressBar.mode = "indeterminate";
     if (myForm.valid) {
-      this.http.post(this.endpoint, myForm.value)
-        .subscribe(
-          (response) => {
-            console.log(response);
-            this.resultColor = "#00ff88";
-            this.submitReport = "Thank up for your Contact. I will replay as soon as possible!";
-            this.submitComplete = true;
-            myForm.reset();
-          },
-          (error) => {
-            console.error(error);
-            this.resultColor = "#ff0077";
-            this.submitReport = "Error occure while sending your message! You can contact me directly on E-mail: crunck78@googlemail.com.";
-            this.submitComplete = true;
-          }
-        );
+      this.sendForm(myForm);
     }
+  }
+
+  sendForm(form) {
+    this.progressBar.mode = "indeterminate";
+    this.http.post(this.endpoint, form.value)
+      .subscribe(
+        response => this.handleResponse(response, form),
+        error => this.handleError(error, form),
+      );
+  }
+
+  handleResponse(response, form) {
+    console.log(response);
+    this.resultColor = "#00ff88";
+    this.submitReport = "Thank up for your Contact. I will replay as soon as possible!";
+    this.submitComplete = true;
+    form.reset();
+  }
+
+  handleError(error, form) {
+    console.error(error);
+    this.resultColor = "#ff0077";
+    this.submitReport = "Error occure while sending your message! You can contact me directly on E-mail: crunck78@googlemail.com.";
+    this.submitComplete = true;
+    form.reset();
   }
 
   closeCard() {
     this.submitComplete = false;
     this.submitReport = '';
     this.progressBar.mode = "determinate";
-  }
-
-  ngOnInit(): void {
-    //Start php via the built in server: $ php -S localhost:8000
-
   }
 }
