81cb3b2b2bbb9a0334f70e0fa9c4ec3555ba1d94
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2022-10-18 09:02:47 +0200

    correct types assigment and footer bottom on small height pages

--- src/app/services/progress-bar.service.ts
+++ src/app/services/progress-bar.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { ProgressBarMode } from '@angular/material/progress-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgressBarService {
 
-  public mode = "determinate";
+  public mode = "determinate" as ProgressBarMode;
 
   constructor() { }
 }
